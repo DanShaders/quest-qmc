76b57a5b1e2bc2b3161d0d2846ae3a3b8efd69e0
Dan Klishch <danilklishch@gmail.com>
2024-08-09 13:25:40 -0400

    Split sema::FreeformGeometry into multiple classes
    This is done to stop uncontrollable growth of it.

--- SRC/sema/FreeformGeometry.cpp
+++ SRC/sema/FreeformGeometry.cpp
@@ -8,15 +8,15 @@ namespace dqmc::sema {
 
 namespace {
 
-std::vector<Vector3i> compute_primitive_cells_in_supercell(FreeformGeometry const& geometry)
+std::vector<Vector3i> compute_primitive_cells_in_supercell(Lattice const& lattice)
 {
     // Compute the number of points with integer fractionary coordinates in a supercell. These will
     // be precisely points we are interested to find explicitly.
-    int det = geometry.supercell_fractionary_basis().determinant();
+    int det = lattice.supercell_fractionary_basis.determinant();
 
-    Matrix3i supercell_basis_inverse; // = (supercell_fractionary_basis)^-1 * det(supercell_fractionary_basis)
+    Matrix3i supercell_basis_inverse; // = (lattice.supercell_fractionary_basis)^-1 * det
     {
-        auto const& m = geometry.supercell_fractionary_basis();
+        auto const& m = lattice.supercell_fractionary_basis;
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
                 supercell_basis_inverse(j, i)
@@ -45,7 +45,9 @@ std::vector<Vector3i> compute_primitive_cells_in_supercell(FreeformGeometry cons
     // supercell. However, this is not the case (consider basis {(3, 1, 0), (5, 1, 0), (0, 0, 1)}).
     // Fortunately, there is an easy fix: if one uses a quotient of the full (infinite) primary
     // lattice graph under equivalence classes induced by superlattice, then every equivalence class
-    // (and, therefore, every integer fractionary point) will be reached.
+    // (and, therefore, every integer fractionary point) will be reached. In practice, this means
+    // that if step out of the supercell when considering an edge, we apply a linear combination of
+    /// supercell basis vectors to the point to get back into the supercell.
     for (size_t i = 0; i < result.size(); ++i) {
         auto site = result[i];
 
@@ -72,7 +74,7 @@ std::vector<Vector3i> compute_primitive_cells_in_supercell(FreeformGeometry cons
     VERIFY(result.size() == det);
 
     for (auto& primitive_cell : result) {
-        primitive_cell = geometry.supercell_fractionary_basis() * primitive_cell / det;
+        primitive_cell = lattice.supercell_fractionary_basis * primitive_cell / det;
     }
 
     // Order primitive cells in the same way as in the legacy code.
@@ -83,19 +85,17 @@ std::vector<Vector3i> compute_primitive_cells_in_supercell(FreeformGeometry cons
     return result;
 }
 
-std::vector<FreeformGeometry::Site> compute_sites(FreeformGeometry const& geometry)
+std::vector<Site> compute_sites(Lattice const& lattice)
 {
-    using Site = FreeformGeometry::Site;
-
     auto translate = [&](Site const& site, Vector3i const& translation) {
-        Vector3d fractionary_coordinates = site.fractionary_position() + translation.cast<f64>();
-        Vector3d cartesian_coordinates = geometry.lattice_basis() * fractionary_coordinates;
-        return Site { site.label(), cartesian_coordinates, fractionary_coordinates };
+        Vector3d fractionary_coordinates = site.fractionary_position + translation.cast<f64>();
+        Vector3d cartesian_coordinates = lattice.basis * fractionary_coordinates;
+        return Site { site.label, cartesian_coordinates, fractionary_coordinates };
     };
 
     std::vector<Site> result;
-    for (Vector3i const& cell : geometry.primitive_cells_in_supercell()) {
-        for (Site const& site : geometry.primitive_cell_sites()) {
+    for (Vector3i const& cell : lattice.primitive_cells_in_supercell) {
+        for (Site const& site : lattice.primitive_cell_sites) {
             result.push_back(translate(site, cell));
         }
     }
@@ -120,8 +120,8 @@ void FreeformGeometry::legacy_compatible_format_into(std::ostream& stream) const
                        " Basic real space geometry info\n"
                        "\n"
                        " Crystal atomic basis\n");
-    for (int i = 0; i < primitive_cell_sites_count(); ++i) {
-        auto const& coords = primitive_cell_sites()[i].fractionary_position();
+    for (int i = 0; i < m_lattice.primitive_cell_sites.size(); ++i) {
+        auto const& coords = m_lattice.primitive_cell_sites[i].fractionary_position;
         std::println(stream, "{:3}{:14.7f}{:14.7f}{:14.7f}", i, coords[0], coords[1], coords[2]);
     }
 
@@ -129,7 +129,7 @@ void FreeformGeometry::legacy_compatible_format_into(std::ostream& stream) const
                        " Basis cell vectors\n");
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            std::print(stream, "{:14.7f}", lattice_basis()(j, i));
+            std::print(stream, "{:14.7f}", m_lattice.basis(j, i));
         }
         std::print(stream, "\n");
     }
@@ -139,7 +139,7 @@ void FreeformGeometry::legacy_compatible_format_into(std::ostream& stream) const
                        " Supercell vectors (fractionary unit)\n");
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            std::print(stream, "{:5}", supercell_fractionary_basis()(j, i));
+            std::print(stream, "{:5}", m_lattice.supercell_fractionary_basis(j, i));
         }
         std::print(stream, "\n");
     }
@@ -148,7 +148,7 @@ void FreeformGeometry::legacy_compatible_format_into(std::ostream& stream) const
                        " Super-Lattice vectors (cartesian)\n");
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            std::print(stream, "{:14.7f}", supercell_cartesian_basis()(j, i));
+            std::print(stream, "{:14.7f}", m_lattice.supercell_cartesian_basis(j, i));
         }
         std::print(stream, "\n");
     }
@@ -157,14 +157,14 @@ void FreeformGeometry::legacy_compatible_format_into(std::ostream& stream) const
                        " ================================================================\n"
                        " Real space lattice\n"
                        "\n");
-    std::print(stream, " Number of orbitals in primitive cell: {:12}\n", primitive_cell_sites_count());
-    std::print(stream, " Total number of orbitals:             {:12}\n", sites_count());
+    std::print(stream, " Number of orbitals in primitive cell: {:12}\n", m_lattice.primitive_cell_sites.size());
+    std::print(stream, " Total number of orbitals:             {:12}\n", m_lattice.sites.size());
     std::print(stream, " index  label   type       X           Y         Z   \n");
-    for (int i = 0; i < sites_count(); ++i) {
-        auto const& site = sites()[i];
-        auto const& coords = site.cartesian_position();
+    for (int i = 0; i < m_lattice.sites.size(); ++i) {
+        auto const& site = m_lattice.sites[i];
+        auto const& coords = site.cartesian_position;
         std::print(stream, "{:3} {:3} {:3}{:14.5f}{:14.5f}{:14.5f}\n",
-            i, site.label(), i % primitive_cell_sites_count(), coords[0], coords[1], coords[2]);
+            i, site.label, i % m_lattice.primitive_cell_sites.size(), coords[0], coords[1], coords[2]);
     }
 }
 
@@ -173,22 +173,24 @@ parser::DiagnosticOr<void> FreeformGeometry::initialize(
     parser::ParsedFreeformGeometry const& geometry,
     parser::ParsedFreeformGeometryParameters const& parameters)
 {
+    // ===== m_lattice initialization =====
     // dimensions
     VERIFY(geometry.dimensions >= 1 && geometry.dimensions <= 3);
-    m_dimensions = geometry.dimensions;
+    m_lattice.dimensions = geometry.dimensions;
+    int dims = m_lattice.dimensions;
 
-    // lattice_basis
+    // basis
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             f64 coefficient = geometry.lattice_basis[i][j];
-            m_lattice_basis(j, i) = coefficient;
-            if (i >= dimensions() || j >= dimensions()) {
+            m_lattice.basis(j, i) = coefficient;
+            if (i >= dims || j >= dims) {
                 VERIFY((i == j && coefficient == 1e3) || (i != j && coefficient == 0));
             }
         }
     }
 
-    if (std::abs(m_lattice_basis.determinant()) < epsilon) {
+    if (std::abs(m_lattice.basis.determinant()) < epsilon) {
         return diag.error(geometry.lattice_basis_location,
             "lattice basis is not linearly independent");
     }
@@ -197,36 +199,36 @@ parser::DiagnosticOr<void> FreeformGeometry::initialize(
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             int coefficient = geometry.supercell_basis[i][j];
-            m_supercell_fractionary_basis(j, i) = coefficient;
-            if (i >= dimensions() || j >= dimensions()) {
+            m_lattice.supercell_fractionary_basis(j, i) = coefficient;
+            if (i >= dims || j >= dims) {
                 VERIFY((i == j && coefficient == 1) || (i != j && coefficient == 0));
             }
         }
     }
 
-    if (supercell_fractionary_basis().determinant() == 0) {
+    if (m_lattice.supercell_fractionary_basis.determinant() == 0) {
         return diag.error(geometry.supercell_basis_location,
             "supercell does not contain any primary cells");
     }
 
     // supercell_cartesian_basis
-    m_supercell_cartesian_basis = lattice_basis() * supercell_fractionary_basis().cast<f64>();
+    m_lattice.supercell_cartesian_basis = m_lattice.basis * m_lattice.supercell_fractionary_basis.cast<f64>();
 
     // primitive_cells_in_supercell
-    m_primitive_cells_in_supercell = compute_primitive_cells_in_supercell(*this);
+    m_lattice.primitive_cells_in_supercell = compute_primitive_cells_in_supercell(m_lattice);
 
     // primitive_cell_sites
-    Matrix3d lattice_basis_inverse = lattice_basis().inverse();
+    Matrix3d lattice_basis_inverse = m_lattice.basis.inverse();
     for (auto const& site : geometry.primitive_cell_sites) {
         Vector3d cartesian_position { site.displacement[0], site.displacement[1], site.displacement[2] };
-        m_primitive_cell_sites.emplace_back(
+        m_lattice.primitive_cell_sites.emplace_back(
             site.label,
             std::move(cartesian_position),
             lattice_basis_inverse * cartesian_position);
     }
 
     // sites
-    m_sites = compute_sites(*this);
+    m_lattice.sites = compute_sites(m_lattice);
 
     return {};
 }
--- SRC/sema/FreeformGeometry.h
+++ SRC/sema/FreeformGeometry.h
@@ -8,51 +8,44 @@
 
 namespace dqmc::sema {
 
-using Eigen::Matrix3d, Eigen::Matrix3i, Eigen::Vector3d, Eigen::Vector3i;
+using Eigen::Matrix3d, Eigen::Matrix3i, Eigen::MatrixXd,
+    Eigen::Vector3d, Eigen::Vector3i;
 
 inline constexpr f64 epsilon = 1e-12;
 
+struct Site {
+    std::string label;
+    Vector3d cartesian_position;
+    Vector3d fractionary_position;
+};
+
+struct Lattice {
+    int dimensions;
+    // Cartesian components of primitive cell (*)
+    Matrix3d basis;
+
+    // Cartesian components of supercell (*)
+    Matrix3d supercell_cartesian_basis;
+    // Fractionary components of supercell (*)
+    Matrix3i supercell_fractionary_basis;
+
+    std::vector<Vector3i> primitive_cells_in_supercell;
+
+    std::vector<Site> primitive_cell_sites;
+
+    std::vector<Site> sites;
+
+    // (*) -- vectors are columns of the matrix
+};
+
 class FreeformGeometry {
 public:
-    class Site {
-    public:
-        Site(std::string label, Vector3d cartesian_position, Vector3d fractionary_position)
-            : m_label(std::move(label))
-            , m_cartesian_position(std::move(cartesian_position))
-            , m_fractionary_position(std::move(fractionary_position))
-        {
-        }
-
-        std::string const& label() const& { return m_label; }
-        Vector3d const& cartesian_position() const& { return m_cartesian_position; }
-        Vector3d const& fractionary_position() const& { return m_fractionary_position; }
-
-    private:
-        std::string m_label;
-        Vector3d m_cartesian_position;
-        Vector3d m_fractionary_position;
-    };
-
     static parser::DiagnosticOr<FreeformGeometry> create(
         parser::DiagnosticEngine& diag,
         parser::ParsedFreeformGeometry const& geometry,
         parser::ParsedFreeformGeometryParameters const& parameters);
 
     void legacy_compatible_format_into(std::ostream& stream) const;
-
-    int dimensions() const { return m_dimensions; }
-    Matrix3d const& lattice_basis() const& { return m_lattice_basis; }
-    Matrix3d const& supercell_cartesian_basis() const& { return m_supercell_cartesian_basis; }
-    Matrix3i const& supercell_fractionary_basis() const& { return m_supercell_fractionary_basis; }
-
-    int primitive_cells_in_supercell_count() const { return m_primitive_cells_in_supercell.size(); }
-    std::vector<Vector3i> const& primitive_cells_in_supercell() const& { return m_primitive_cells_in_supercell; }
-
-    int primitive_cell_sites_count() const { return m_primitive_cell_sites.size(); }
-    std::vector<Site> const& primitive_cell_sites() const { return m_primitive_cell_sites; }
-
-    int sites_count() const { return m_sites.size(); }
-    std::vector<Site> const& sites() const { return m_sites; }
 
 private:
     FreeformGeometry() = default;
@@ -62,22 +55,7 @@ private:
         parser::ParsedFreeformGeometry const& geometry,
         parser::ParsedFreeformGeometryParameters const& parameters);
 
-    int m_dimensions = 0;
-    // Cartesian components of primitive cell (*)
-    Matrix3d m_lattice_basis;
-
-    // Cartesian components of supercell (*)
-    Matrix3d m_supercell_cartesian_basis;
-    // Fractionary components of supercell (*)
-    Matrix3i m_supercell_fractionary_basis;
-
-    std::vector<Vector3i> m_primitive_cells_in_supercell;
-
-    std::vector<Site> m_primitive_cell_sites;
-
-    std::vector<Site> m_sites;
-
-    // (*) -- vectors are columns of the matrix
+    Lattice m_lattice;
 };
 
 } // namespace dqmc::sema
