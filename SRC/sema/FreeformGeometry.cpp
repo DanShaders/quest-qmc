666f17a025f2daf9daaa2937ea3052f14ef639f5
Dan Klishch <danilklishch@gmail.com>
2024-08-12 11:41:38 -0400

    Break up FreeformGeometry::create into multiple functions

--- SRC/sema/FreeformGeometry.cpp
+++ SRC/sema/FreeformGeometry.cpp
@@ -110,6 +110,172 @@ std::vector<Site> compute_sites(Lattice const& lattice)
     return result;
 }
 
+parser::DiagnosticOr<void> initialize_lattice(
+    parser::DiagnosticEngine& diag,
+    Lattice& lattice,
+    parser::ParsedFreeformGeometry const& geometry)
+{
+    // dimensions
+    VERIFY(geometry.dimensions >= 1 && geometry.dimensions <= 3);
+    lattice.dimensions = geometry.dimensions;
+    int dims = lattice.dimensions;
+
+    // basis
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            f64 coefficient = geometry.lattice_basis[i][j];
+            lattice.basis(j, i) = coefficient;
+            if (i >= dims || j >= dims) {
+                VERIFY((i == j && coefficient == 1e3) || (i != j && coefficient == 0));
+            }
+        }
+    }
+
+    if (std::abs(lattice.basis.determinant()) < epsilon) {
+        return diag.error(geometry.lattice_basis_location,
+            "lattice basis is not linearly independent");
+    }
+
+    // supercell_fractionary_basis
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            int coefficient = geometry.supercell_basis[i][j];
+            lattice.supercell_fractionary_basis(j, i) = coefficient;
+            if (i >= dims || j >= dims) {
+                VERIFY((i == j && coefficient == 1) || (i != j && coefficient == 0));
+            }
+        }
+    }
+
+    if (lattice.supercell_fractionary_basis.determinant() == 0) {
+        return diag.error(geometry.supercell_basis_location,
+            "supercell does not contain any primary cells");
+    }
+
+    // supercell_cartesian_basis
+    lattice.supercell_cartesian_basis = lattice.basis * lattice.supercell_fractionary_basis.cast<f64>();
+
+    // primitive_cells_in_supercell
+    lattice.primitive_cells_in_supercell = compute_primitive_cells_in_supercell(lattice);
+
+    // primitive_cell_sites
+    Matrix3d lattice_basis_inverse = lattice.basis.inverse();
+    for (auto const& site : geometry.primitive_cell_sites) {
+        Vector3d cartesian_position { site.displacement[0], site.displacement[1], site.displacement[2] };
+        lattice.primitive_cell_sites.emplace_back(
+            site.label,
+            std::move(cartesian_position),
+            lattice_basis_inverse * cartesian_position);
+    }
+
+    // sites
+    lattice.sites = compute_sites(lattice);
+
+    return {};
+}
+
+parser::DiagnosticOr<void> initialize_hamiltonian(
+    parser::DiagnosticEngine& diag,
+    Hamiltonian& hamiltonian,
+    Lattice const& lattice,
+    parser::ParsedFreeformGeometry const& geometry,
+    parser::ParsedFreeformGeometryParameters const& parameters)
+{
+    Matrix3d lattice_basis_inverse = lattice.basis.inverse();
+    int cell_site_count = lattice.primitive_cell_sites.size();
+    int total_site_count = lattice.sites.size();
+    int cells_count = lattice.primitive_cells_in_supercell.size();
+
+    hamiltonian.interactions.resize(total_site_count);
+    for (auto& site : hamiltonian.interactions) {
+        site.mu_up = parameters.mu_up;
+        site.mu_down = parameters.mu_down;
+    }
+
+    hamiltonian.hoppings[0] = MatrixXd::Zero(total_site_count, total_site_count);
+    hamiltonian.hoppings[1] = MatrixXd::Zero(total_site_count, total_site_count);
+
+    auto [supercell_size, supercell_basis_inverse] = compute_premultiplied_inverse(lattice.supercell_fractionary_basis);
+
+    std::map<Vector3i, int, Vector3iComparator> index_of_primitive_cell;
+    for (int i = 0; i < cells_count; ++i) {
+        index_of_primitive_cell[lattice.primitive_cells_in_supercell[i]] = i;
+    }
+
+    auto process_on_site_interaction = [&](parser::ParsedFreeformGeometry::OnSiteInteraction const& interaction) -> parser::DiagnosticOr<void> {
+        for (int cell = 0; cell < cells_count; ++cell) {
+            int site_index = cell * cell_site_count + interaction.site;
+            auto& site = hamiltonian.interactions[site_index];
+            site.u += interaction.u;
+            site.mu_up -= interaction.mu_up_offset;
+            site.mu_down -= interaction.mu_down_offset;
+        }
+        return {};
+    };
+
+    auto process_hopping = [&](parser::ParsedFreeformGeometry::Hopping const& hopping) -> parser::DiagnosticOr<void> {
+        Vector3d fractionary_shift_fp = lattice.sites[hopping.to].cartesian_position
+            - (lattice.sites[hopping.from].cartesian_position + Vector3d { hopping.coordinate_delta });
+        fractionary_shift_fp = lattice_basis_inverse * fractionary_shift_fp;
+
+        Vector3i fractionary_shift = fractionary_shift_fp.cast<int>();
+
+        double badness = (fractionary_shift.cast<f64>() - fractionary_shift_fp).squaredNorm();
+
+        if (badness > epsilon) {
+            return diag.error(hopping.location,
+                "distance {:.1e} (in supercell lattice basis) to the best candidate for the "
+                "mentioned site is larger than allowed {:.1e} rounding error",
+                sqrt(badness), sqrt(epsilon));
+        }
+
+        for (int from_cell_index = 0; from_cell_index < cells_count; ++from_cell_index) {
+            auto from_cell = lattice.primitive_cells_in_supercell[from_cell_index];
+            auto to_cell = from_cell + fractionary_shift;
+
+            Vector3i to_coordinates = supercell_basis_inverse * to_cell;
+            bool should_negate_phase = false;
+            for (int component = 0; component < 3; ++component) {
+                int coordinate = to_coordinates[component];
+                int coordinate_inside_supercell = coordinate % supercell_size;
+                if (coordinate_inside_supercell < 0) {
+                    coordinate_inside_supercell += supercell_size;
+                }
+                to_coordinates[component] = coordinate_inside_supercell;
+
+                int phase_power = (coordinate - coordinate_inside_supercell) / supercell_size;
+                should_negate_phase ^= (phase_power * parameters.should_negate_phase[component]) & 1;
+            }
+
+            to_coordinates = lattice.supercell_fractionary_basis * to_coordinates / supercell_size;
+            int to_cell_index = index_of_primitive_cell.at(to_coordinates);
+
+            int from_site = from_cell_index * cell_site_count + hopping.from;
+            int to_site = to_cell_index * cell_site_count + hopping.to;
+
+            if (from_site == to_site) {
+                return diag.error(hopping.location,
+                    "hopping terms with equal endpoints in quotient of the lattice by "
+                    "supercell are not implemented, shift chemical potential instead");
+            }
+
+            int phase_shift = should_negate_phase ? -1 : 1;
+
+            hamiltonian.hoppings[0](from_site, to_site) += hopping.t_up * phase_shift;
+            hamiltonian.hoppings[0](to_site, from_site) += hopping.t_up * phase_shift;
+            hamiltonian.hoppings[1](to_site, from_site) += hopping.t_down * phase_shift;
+            hamiltonian.hoppings[1](from_site, to_site) += hopping.t_down * phase_shift;
+        }
+
+        return {};
+    };
+
+    for (auto const& term : geometry.hamiltonian) {
+        TRY(visit(term, process_hopping, process_on_site_interaction));
+    }
+    return {};
+}
+
 } // namespace
 
 parser::DiagnosticOr<FreeformGeometry> FreeformGeometry::create(
@@ -181,162 +347,8 @@ parser::DiagnosticOr<void> FreeformGeometry::initialize(
     parser::ParsedFreeformGeometry const& geometry,
     parser::ParsedFreeformGeometryParameters const& parameters)
 {
-    // ===== m_lattice initialization =====
-    // dimensions
-    VERIFY(geometry.dimensions >= 1 && geometry.dimensions <= 3);
-    m_lattice.dimensions = geometry.dimensions;
-    int dims = m_lattice.dimensions;
-
-    // basis
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            f64 coefficient = geometry.lattice_basis[i][j];
-            m_lattice.basis(j, i) = coefficient;
-            if (i >= dims || j >= dims) {
-                VERIFY((i == j && coefficient == 1e3) || (i != j && coefficient == 0));
-            }
-        }
-    }
-
-    if (std::abs(m_lattice.basis.determinant()) < epsilon) {
-        return diag.error(geometry.lattice_basis_location,
-            "lattice basis is not linearly independent");
-    }
-
-    // supercell_fractionary_basis
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            int coefficient = geometry.supercell_basis[i][j];
-            m_lattice.supercell_fractionary_basis(j, i) = coefficient;
-            if (i >= dims || j >= dims) {
-                VERIFY((i == j && coefficient == 1) || (i != j && coefficient == 0));
-            }
-        }
-    }
-
-    if (m_lattice.supercell_fractionary_basis.determinant() == 0) {
-        return diag.error(geometry.supercell_basis_location,
-            "supercell does not contain any primary cells");
-    }
-
-    // supercell_cartesian_basis
-    m_lattice.supercell_cartesian_basis = m_lattice.basis * m_lattice.supercell_fractionary_basis.cast<f64>();
-
-    // primitive_cells_in_supercell
-    m_lattice.primitive_cells_in_supercell = compute_primitive_cells_in_supercell(m_lattice);
-
-    // primitive_cell_sites
-    Matrix3d lattice_basis_inverse = m_lattice.basis.inverse();
-    for (auto const& site : geometry.primitive_cell_sites) {
-        Vector3d cartesian_position { site.displacement[0], site.displacement[1], site.displacement[2] };
-        m_lattice.primitive_cell_sites.emplace_back(
-            site.label,
-            std::move(cartesian_position),
-            lattice_basis_inverse * cartesian_position);
-    }
-
-    // sites
-    m_lattice.sites = compute_sites(m_lattice);
-
-    // ===== m_hamiltonian initialization =====
-    int cell_site_count = m_lattice.primitive_cell_sites.size();
-    int total_site_count = m_lattice.sites.size();
-    int cells_count = m_lattice.primitive_cells_in_supercell.size();
-
-    m_hamiltonian.interactions.resize(total_site_count);
-    for (auto& site : m_hamiltonian.interactions) {
-        site.mu_up = parameters.mu_up;
-        site.mu_down = parameters.mu_down;
-    }
-
-    m_hamiltonian.hoppings[0] = MatrixXd::Zero(total_site_count, total_site_count);
-    m_hamiltonian.hoppings[1] = MatrixXd::Zero(total_site_count, total_site_count);
-
-    auto [supercell_size, supercell_basis_inverse] = compute_premultiplied_inverse(m_lattice.supercell_fractionary_basis);
-
-    std::map<Vector3i, int, Vector3iComparator> index_of_primitive_cell;
-    for (int i = 0; i < cells_count; ++i) {
-        index_of_primitive_cell[m_lattice.primitive_cells_in_supercell[i]] = i;
-    }
-
-    for (auto const& term : geometry.hamiltonian) {
-        bool issued_diagnostic = false;
-
-        visit(
-            term,
-            [&](parser::ParsedFreeformGeometry::OnSiteInteraction const& interaction) {
-                for (int cell = 0; cell < cells_count; ++cell) {
-                    int site_index = cell * cell_site_count + interaction.site;
-                    auto& site = m_hamiltonian.interactions[site_index];
-                    site.u += interaction.u;
-                    site.mu_up -= interaction.mu_up_offset;
-                    site.mu_down -= interaction.mu_down_offset;
-                }
-            },
-            [&](parser::ParsedFreeformGeometry::Hopping const& hopping) {
-                Vector3d fractionary_shift_fp = m_lattice.sites[hopping.to].cartesian_position
-                    - (m_lattice.sites[hopping.from].cartesian_position + Vector3d { hopping.coordinate_delta });
-                fractionary_shift_fp = lattice_basis_inverse * fractionary_shift_fp;
-
-                Vector3i fractionary_shift = fractionary_shift_fp.cast<int>();
-
-                double badness = (fractionary_shift.cast<f64>() - fractionary_shift_fp).squaredNorm();
-
-                if (badness > epsilon) {
-                    diag.error(hopping.location,
-                        "distance {:.1e} (in supercell lattice basis) to the best candidate for the "
-                        "mentioned site is larger than allowed {:.1e} rounding error",
-                        sqrt(badness), sqrt(epsilon));
-                    issued_diagnostic = true;
-                    return;
-                }
-
-                for (int from_cell_index = 0; from_cell_index < cells_count; ++from_cell_index) {
-                    auto from_cell = m_lattice.primitive_cells_in_supercell[from_cell_index];
-                    auto to_cell = from_cell + fractionary_shift;
-
-                    Vector3i to_coordinates = supercell_basis_inverse * to_cell;
-                    bool should_negate_phase = false;
-                    for (int component = 0; component < 3; ++component) {
-                        int coordinate = to_coordinates[component];
-                        int coordinate_inside_supercell = coordinate % supercell_size;
-                        if (coordinate_inside_supercell < 0) {
-                            coordinate_inside_supercell += supercell_size;
-                        }
-                        to_coordinates[component] = coordinate_inside_supercell;
-
-                        int phase_power = (coordinate - coordinate_inside_supercell) / supercell_size;
-                        should_negate_phase ^= (phase_power * parameters.should_negate_phase[component]) & 1;
-                    }
-
-                    to_coordinates = m_lattice.supercell_fractionary_basis * to_coordinates / supercell_size;
-                    int to_cell_index = index_of_primitive_cell.at(to_coordinates);
-
-                    int from_site = from_cell_index * cell_site_count + hopping.from;
-                    int to_site = to_cell_index * cell_site_count + hopping.to;
-
-                    if (from_site == to_site) {
-                        diag.error(hopping.location,
-                            "hopping terms with equal endpoints in quotient of the lattice by "
-                            "supercell are not implemented, shift chemical potential instead");
-                        issued_diagnostic = true;
-                        return;
-                    }
-
-                    int phase_shift = should_negate_phase ? -1 : 1;
-
-                    m_hamiltonian.hoppings[0](from_site, to_site) += hopping.t_up * phase_shift;
-                    m_hamiltonian.hoppings[0](to_site, from_site) += hopping.t_up * phase_shift;
-                    m_hamiltonian.hoppings[1](to_site, from_site) += hopping.t_down * phase_shift;
-                    m_hamiltonian.hoppings[1](from_site, to_site) += hopping.t_down * phase_shift;
-                }
-            });
-
-        if (issued_diagnostic) {
-            return std::unexpected<parser::DiagnosedError> { {} };
-        }
-    }
-
+    TRY(initialize_lattice(diag, m_lattice, geometry));
+    TRY(initialize_hamiltonian(diag, m_hamiltonian, m_lattice, geometry, parameters));
     return {};
 }
 
